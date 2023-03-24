dc8ff9ab83c489176c0dc49a00e9987f5dda0f31
HT-Tuan <huynhthanhtuan2206@gmail.com>
2023-03-24 14:06:15 +0700

    cập nhật thêm giao diện

---
+++ MusicApplication/app/src/main/java/com/example/musicapplication/Activity_splash.java
@@ -0,0 +1,30 @@
+package com.example.musicapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity_splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        loadData();
+    }
+
+    private void loadData() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Activity_splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
