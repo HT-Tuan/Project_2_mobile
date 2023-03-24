ad0aafd2532181acf0929c5b6ad47fe75427e1cc
Văn Dân <danok123456@gmail.com>
2023-03-24 10:48:19 +0700

    finish design login page, sign up page

--- MusicApplication/app/src/main/java/com/example/musicapplication/MainActivity.java
+++ MusicApplication/app/src/main/java/com/example/musicapplication/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_canhan);
+        setContentView(R.layout.activity_playing_music);
     }
 }
